cbe8f3ab10f73b5caaf6d7f76892a2441617fb43
MF2018 <35531763+MF2018@users.noreply.github.com>
2018-01-20 19:00:15 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -5,9 +5,17 @@ Created on Wed Jan 17 18:32:16 2018
 @author: Dela
 """
 """
-This main function uses pyxlsDownloader to search furst for Download-elements at an homepage. 
+This main function uses pyxlsDownloader to search for Download-elements of a homepage. 
 Then it downloads the link in a second step.
-In a third step it uses the statisticAnalyser to analyse the Data and plot it. 
+In a third step it uses the Analyser to analyse the Data and plot a part of the data.
+In a third step the Analyser uploads the Data to Google Drive using an API client. The google upload bases on the module of
+google_drive_util.py on github:https://gist.github.com/lheric/876a924c5d77bde0f62526c7fa6ad846 .
+To use the API client the users has to generate 'client_secrets.json' from https://console.developers.google.com/ . 
+
+The code of the main function is only rudimental. In further step a gui will be provided. 
+So, the user can enter a homepage of his wish and can choose more then von download link.
+After that he can choose the columns for the data-analysis and which data he wants to plot.
+But this will be in a further step
 """
 from CSV_download import pyxlsDownloader 
 import os
